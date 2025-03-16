ce7a9814549128823b5d3c9a53a8efeb23ad4b04
maheshkumar.patil <maheshkumar.patil@LPT3206.newgen.co.in>
2025-03-17 04:58:05 +0530

    first program

---
+++ src/simpletest/FirstProgram.java
@@ -0,0 +1,10 @@
+package simpletest;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello git");
+	}
+
+}
